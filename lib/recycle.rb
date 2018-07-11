6da6d0e328a562f04bfd624fb2014a9fb5be027f
Roberto Gaitan <rgaitan.it@gmail.com>
2018-07-11 11:38:40 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -31,8 +31,9 @@ end
 #This method should print out each key in the hash.
 #You should use the `each_key` method to iterate over your hash and print out each key.
 def all_keys(first_recycling_bin)
-  first_recycling_bin.each_key[key]
+  first_recycling_bin.each_key{[key]
   puts "#{key}"
+}
 end
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
